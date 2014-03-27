da815a30aa4b8c8780bfff89f4e0f9f44c86c115
thiagoabreu <thiagoa7@gmail.com>
2014-03-26 23:24:38 -0300

    Initial dev commit

---
+++ Horarios.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Circular
+{
+    [XmlRoot(ElementName = "direto", Namespace = "")]
+    public class Direto
+    {
+        [XmlElement(ElementName = "String", Namespace = "")]
+        public List<String> hora;
+    }
+
+    [XmlRoot(ElementName = "inverso", Namespace = "")]
+    public class Inverso
+    {
+        [XmlElement(ElementName = "String", Namespace = "")]
+        public List<String> hora;
+    }
+
+    [XmlRoot(ElementName = "dia", Namespace = "")]
+    public class Dia
+    {
+        [XmlElement(ElementName = "nome", Namespace = "")]
+        public String nome;
+        [XmlElement(ElementName = "direto", Namespace = "")]
+        public Direto direto;
+        [XmlElement(ElementName = "inverso", Namespace = "")]
+        public Inverso inverso;
+    }
+
+    [XmlRoot(ElementName = "horarios", Namespace = "")]
+    public class Horarios
+    {
+        [XmlElement(ElementName = "dia", Namespace = "")]
+        public List<Dia> dia;
+
+
+    }
+}
